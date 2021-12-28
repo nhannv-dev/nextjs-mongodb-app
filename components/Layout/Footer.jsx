01a4ecef7f74d3072c19bdc1d9f06db113180ffd
nhannv-dev <nhannv95.it@gmail.com>
2021-12-28 17:44:31 +0700

    Initial commit

---
+++ components/Layout/Footer.jsx
@@ -0,0 +1,25 @@
+import { Text, TextLink } from '@/components/Text'
+import { ThemeSwitcher } from '@/components/ThemeSwitcher'
+import styles from './Footer.module.css'
+import Spacer from './Spacer'
+import Wrapper from './Wrapper'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Wrapper>
+        <Text color="accents-7">
+          Made with ❤️, 🔥, and a updated by{' '}
+          <TextLink href="https://nhannv-dev.github.io/mycv/" color="link">
+            Nhan Nguyen
+          </TextLink>
+          .
+        </Text>
+        <Spacer size={1} axis="vertical" />
+        <ThemeSwitcher />
+      </Wrapper>
+    </footer>
+  )
+}
+
+export default Footer
